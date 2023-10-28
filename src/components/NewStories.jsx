843317bbf4f8d1795d90a537d8890ae65dade8e1
Michelle Brown <m.andreabr@gmail.com>
2023-10-28 17:25:00 +1100

    create new stories component

---
+++ src/components/NewStories.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function NewStories() {
+    return (
+        <article className="new-stories">
+            <h2>New</h2>
+
+            <h3>Hydrogen VS Electric Cars</h3>
+            <p>Will hydrogen-fueled cars ever catch up to EVs?</p>
+
+            <h3>The Downsides of AI Artistry</h3>
+            <p>What are the possible adverse effects of on-demand AI image generation?</p>
+
+            <h3>Is VC Funding Drying Up?</h3>
+            <p>Private funding by VC firms is down 50% YOY. We take a look at what that means.</p>
+        </article>
+    )
+}
