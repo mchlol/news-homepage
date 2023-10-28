6142e1b6efa24fc7848a36877b77f94f0032f7cd
Michelle Brown <m.andreabr@gmail.com>
2023-10-28 19:00:41 +1100

    remove max width property

--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -9,7 +9,7 @@ export default function Featured() {
         <section className="featured">
             <div className="featured--content">
                 <img className="featured--img" src={img1} alt=""/>
-                <div class="featured--text">
+                <div className="featured--text">
                     <h2>01</h2>
                     <h3>Reviving Retro PCs</h3>
                     <p>What happens when old PCs are given modern upgrades?</p>
@@ -18,7 +18,7 @@ export default function Featured() {
 
             <div className="featured--content">
                 <img className="featured--img" src={img2} alt="" />
-                <div class="featured--text">
+                <div className="featured--text">
                     <h2>02</h2>
                     <h3>Top 10 Laptops of 2022</h3>
                     <p>Our best picks for various needs and budgets.</p>
@@ -27,7 +27,7 @@ export default function Featured() {
 
             <div className="featured--content">
                 <img className="featured--img" src={img3} alt="" />
-                <div class="featured--text">
+                <div className="featured--text">
                     <h2>03</h2>
                     <h3>The Growth of Gaming</h3>
                     <p>How the pandemic has sparked fresh opportunities.</p>
