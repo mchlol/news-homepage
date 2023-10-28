bf6e75d138ca8db8e8c8f8338ceea1ad55d51987
Michelle Brown <m.andreabr@gmail.com>
2023-10-28 17:25:19 +1100

    position components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import './App.css'
 import NavBar from './components/NavBar'
+import LeadArticle from './components/LeadArticle'
+import NewStories from './components/NewStories'
 
 function App() {
 
   return (
     <>
       <NavBar />
+      <main>
+        {/* <section className="hero"> */}
+          <LeadArticle />
+          <NewStories />
+        {/* </section> */}
+      </main>
     </>
   )
 }
