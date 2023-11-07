0061bc9a6691b43615d4f1596030b9eefdb5db75
Michelle Brown <m.andreabr@gmail.com>
2023-11-07 17:05:54 +1100

    change svg burger for material symbols

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -19,18 +19,9 @@ export default function NavBar() {
                 setMobileNavOpen(!mobileNavOpen);
             }}
             >
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-5 w-5"
-                    viewBox="0 0 20 20"
-                    fill="#e9ab53"
-                >
-                    <path
-                        fillRule="evenodd"
-                        d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z"
-                        clipRule="evenodd"
-                    />
-                </svg>
+                <span class="material-symbols-outlined">
+                    menu
+                </span>
             </button>
 
             <div className={mobileNavOpen ? 'nav-menu visible' : 'nav-menu hidden'}>
